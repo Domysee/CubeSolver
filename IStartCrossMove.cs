a6d4e74a9e907ad5962d07e911ebf7a9e5a203d3
domysee <me@dominik-weber.name>
2015-04-22 11:18:01 +0100

    renamed IMove to IStartCrossMove changed interface specifically to the first step of cube solving (the cross)

--- IStartCrossMove.cs
+++ IStartCrossMove.cs
@@ -9,24 +9,26 @@ namespace RubiksCubeSolver
 	/// <summary>
 	/// represents a set of moves
 	/// </summary>
-	public interface IMove
+	public interface IStartCrossMove
 	{
-		public string Name { get; }
-
 		/// <summary>
 		/// applies the move onto the given cube
+		/// The middle field of the startside and the given side define which edge piece to solve
 		/// </summary>
 		/// <param name="cube"></param>
-		public void Apply(Cube cube);
+		/// <param name="side"></param>
+		public void Apply(Cube cube, Side side);
 
 		/// <summary>
 		/// returns a applicability factor 
 		/// 1 = this move will solve the cube
 		/// 0 = this move will unsolve the cube
 		/// 0.5 = this move will do nothing
+		/// The middle field of the startside and the given side define on which edge piece the applicability is measured
 		/// </summary>
 		/// <param name="cube"></param>
+		/// <param name="side"></param>
 		/// <returns></returns>
-		public double Applicable(Cube cube);
+		public double Applicable(Cube cube, Side side);
 	}
 }
