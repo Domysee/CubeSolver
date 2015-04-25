3d0302654b782ebc55f9540fe007cef6166c2197
domysee <me@dominik-weber.name>
2015-04-25 12:25:30 +0100

    moved GetRelativeSide() and GetSideRelation() from Cube to Helper added Helper.GetRotationNeutralRelativeSidePosition()

--- Helper.cs
+++ Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,7 +54,7 @@ namespace RubiksCubeSolver
 		/// <param name="relativeSidePosition"></param>
 		/// <param name="relativeEdgePosition"></param>
 		/// <returns></returns>
-		public static Side GetRotationNeutralRelativeSide(Side startSide, 
+		public static Side GetRotationNeutralRelativeSide(Side startSide,
 			RelativeSidePosition relativeSidePosition, RelativeEdgePosition relativeEdgePosition)
 		{
 			if (relativeEdgePosition == RelativeEdgePosition.Left)
@@ -94,6 +95,246 @@ namespace RubiksCubeSolver
 			}
 
 			return null;
+		}
+
+		public static RelativeSidePosition GetRotationNeutralRelativeSidePosition(Side startSide,
+			RelativeSidePosition relativeSidePosition, RelativeEdgePosition relativeEdgePosition)
+		{
+			return GetSideRelation(startSide.CubeSide, GetRotationNeutralRelativeSide(startSide, relativeSidePosition, relativeEdgePosition).CubeSide);
+		}
+
+		public static RelativeSidePosition GetSideRelation(Sides startSide, Sides relationSide)
+		{
+			if (startSide == Sides.Front)
+			{
+				if (relationSide == Sides.Front)
+				{
+					return RelativeSidePosition.Self;
+				}
+				if (relationSide == Sides.Back)
+				{
+					return RelativeSidePosition.Opposite;
+				}
+				if (relationSide == Sides.Left)
+				{
+					return RelativeSidePosition.Left;
+				}
+				if (relationSide == Sides.Right)
+				{
+					return RelativeSidePosition.Right;
+				}
+				if (relationSide == Sides.Top)
+				{
+					return RelativeSidePosition.Top;
+				}
+				if (relationSide == Sides.Bottom)
+				{
+					return RelativeSidePosition.Bottom;
+				}
+			}
+
+			if (startSide == Sides.Left)
+			{
+				if (relationSide == Sides.Front)
+				{
+					return RelativeSidePosition.Right;
+				}
+				if (relationSide == Sides.Back)
+				{
+					return RelativeSidePosition.Left;
+				}
+				if (relationSide == Sides.Left)
+				{
+					return RelativeSidePosition.Self;
+				}
+				if (relationSide == Sides.Right)
+				{
+					return RelativeSidePosition.Opposite;
+				}
+				if (relationSide == Sides.Top)
+				{
+					return RelativeSidePosition.Top;
+				}
+				if (relationSide == Sides.Bottom)
+				{
+					return RelativeSidePosition.Bottom;
+				}
+			}
+
+			if (startSide == Sides.Right)
+			{
+				if (relationSide == Sides.Front)
+				{
+					return RelativeSidePosition.Left;
+				}
+				if (relationSide == Sides.Back)
+				{
+					return RelativeSidePosition.Right;
+				}
+				if (relationSide == Sides.Left)
+				{
+					return RelativeSidePosition.Opposite;
+				}
+				if (relationSide == Sides.Right)
+				{
+					return RelativeSidePosition.Self;
+				}
+				if (relationSide == Sides.Top)
+				{
+					return RelativeSidePosition.Top;
+				}
+				if (relationSide == Sides.Bottom)
+				{
+					return RelativeSidePosition.Bottom;
+				}
+			}
+
+			if (startSide == Sides.Back)
+			{
+				if (relationSide == Sides.Front)
+				{
+					return RelativeSidePosition.Opposite;
+				}
+				if (relationSide == Sides.Back)
+				{
+					return RelativeSidePosition.Self;
+				}
+				if (relationSide == Sides.Left)
+				{
+					return RelativeSidePosition.Right;
+				}
+				if (relationSide == Sides.Right)
+				{
+					return RelativeSidePosition.Left;
+				}
+				if (relationSide == Sides.Top)
+				{
+					return RelativeSidePosition.Top;
+				}
+				if (relationSide == Sides.Bottom)
+				{
+					return RelativeSidePosition.Bottom;
+				}
+			}
+
+			if (startSide == Sides.Top)
+			{
+				if (relationSide == Sides.Front)
+				{
+					return RelativeSidePosition.Bottom;
+				}
+				if (relationSide == Sides.Back)
+				{
+					return RelativeSidePosition.Top;
+				}
+				if (relationSide == Sides.Left)
+				{
+					return RelativeSidePosition.Left;
+				}
+				if (relationSide == Sides.Right)
+				{
+					return RelativeSidePosition.Right;
+				}
+				if (relationSide == Sides.Top)
+				{
+					return RelativeSidePosition.Self;
+				}
+				if (relationSide == Sides.Bottom)
+				{
+					return RelativeSidePosition.Opposite;
+				}
+			}
+
+			if (startSide == Sides.Bottom)
+			{
+				if (relationSide == Sides.Front)
+				{
+					return RelativeSidePosition.Top;
+				}
+				if (relationSide == Sides.Back)
+				{
+					return RelativeSidePosition.Bottom;
+				}
+				if (relationSide == Sides.Left)
+				{
+					return RelativeSidePosition.Left;
+				}
+				if (relationSide == Sides.Right)
+				{
+					return RelativeSidePosition.Right;
+				}
+				if (relationSide == Sides.Top)
+				{
+					return RelativeSidePosition.Opposite;
+				}
+				if (relationSide == Sides.Bottom)
+				{
+					return RelativeSidePosition.Self;
+				}
+			}
+
+			return RelativeSidePosition.NotExisting;	//default to satisfy the compiler, should never occur
+		}
+
+		public static Sides GetRelativeSide(Sides startSide, RelativeSidePosition relativeSide)
+		{
+			Debug.Assert(relativeSide != RelativeSidePosition.NotExisting);
+
+			if (startSide == Sides.Front)
+			{
+				if (relativeSide == RelativeSidePosition.Self) return Sides.Front;
+				if (relativeSide == RelativeSidePosition.Opposite) return Sides.Back;
+				if (relativeSide == RelativeSidePosition.Left) return Sides.Left;
+				if (relativeSide == RelativeSidePosition.Right) return Sides.Right;
+				if (relativeSide == RelativeSidePosition.Top) return Sides.Top;
+				if (relativeSide == RelativeSidePosition.Bottom) return Sides.Bottom;
+			}
+			if (startSide == Sides.Back)
+			{
+				if (relativeSide == RelativeSidePosition.Self) return Sides.Back;
+				if (relativeSide == RelativeSidePosition.Opposite) return Sides.Front;
+				if (relativeSide == RelativeSidePosition.Left) return Sides.Right;
+				if (relativeSide == RelativeSidePosition.Right) return Sides.Left;
+				if (relativeSide == RelativeSidePosition.Top) return Sides.Top;
+				if (relativeSide == RelativeSidePosition.Bottom) return Sides.Bottom;
+			}
+			if (startSide == Sides.Left)
+			{
+				if (relativeSide == RelativeSidePosition.Self) return Sides.Left;
+				if (relativeSide == RelativeSidePosition.Opposite) return Sides.Right;
+				if (relativeSide == RelativeSidePosition.Left) return Sides.Back;
+				if (relativeSide == RelativeSidePosition.Right) return Sides.Front;
+				if (relativeSide == RelativeSidePosition.Top) return Sides.Top;
+				if (relativeSide == RelativeSidePosition.Bottom) return Sides.Bottom;
+			}
+			if (startSide == Sides.Right)
+			{
+				if (relativeSide == RelativeSidePosition.Self) return Sides.Right;
+				if (relativeSide == RelativeSidePosition.Opposite) return Sides.Left;
+				if (relativeSide == RelativeSidePosition.Left) return Sides.Front;
+				if (relativeSide == RelativeSidePosition.Right) return Sides.Back;
+				if (relativeSide == RelativeSidePosition.Top) return Sides.Top;
+				if (relativeSide == RelativeSidePosition.Bottom) return Sides.Bottom;
+			}
+			if (startSide == Sides.Top)
+			{
+				if (relativeSide == RelativeSidePosition.Self) return Sides.Top;
+				if (relativeSide == RelativeSidePosition.Opposite) return Sides.Bottom;
+				if (relativeSide == RelativeSidePosition.Left) return Sides.Left;
+				if (relativeSide == RelativeSidePosition.Right) return Sides.Right;
+				if (relativeSide == RelativeSidePosition.Top) return Sides.Back;
+				if (relativeSide == RelativeSidePosition.Bottom) return Sides.Front;
+			}
+			if (startSide == Sides.Bottom)
+			{
+				if (relativeSide == RelativeSidePosition.Self) return Sides.Bottom;
+				if (relativeSide == RelativeSidePosition.Opposite) return Sides.Top;
+				if (relativeSide == RelativeSidePosition.Left) return Sides.Left;
+				if (relativeSide == RelativeSidePosition.Right) return Sides.Right;
+				if (relativeSide == RelativeSidePosition.Top) return Sides.Front;
+				if (relativeSide == RelativeSidePosition.Bottom) return Sides.Back;
+			}
+			return startSide;	//default value to satisfy the compiler, should never be used
 		}
 	}
 }
