fb982ad40f1c766e5dc50c425e04ddaab63c51de
domysee <me@dominik-weber.name>
2015-04-25 16:29:52 +0100

    added StartCrossMove5

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -397,7 +397,7 @@ namespace RubiksCubeSolver
 		{
 			test = new RelayCommand((param) =>
 			{
-				var move = new StartCrossMove4();
+				var move = new StartCrossMove5();
 				foreach (RelativeSidePosition relativeSidePosition in Enum.GetValues(typeof(RelativeSidePosition)))
 				{
 					if (relativeSidePosition != RelativeSidePosition.NotExisting && 
---
+++ StartCrossMoves/StartCrossMove5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCubeSolver.StartCrossMoves
+{
+	/// <summary>
+	/// represents the situation where the edge is in the same position as in StartCrossMove2, but rotated wrongly
+	/// </summary>
+	public class StartCrossMove5 : IStartCrossMove
+	{
+		public void Apply(Cube cube, RelativeSidePosition side)
+		{
+			cube.RotateTopCW();
+			cube.RotateSideCCW(Helper.GetRelativeSide(Helper.GetRelativeSide(Sides.Top, side), RelativeSidePosition.Left));
+			cube.RotateTopCCW();
+		}
+
+		public double Applicable(Cube cube, RelativeSidePosition side)
+		{
+			RelativeSidePosition relativeSidePosition;
+			RelativeEdgePosition relativeEdgePosition;
+			cube.GetRelativeEdgePosition(cube.Top.CubeSide, cube.Top.Color, cube.Top.GetRelativeSide(side).Color,
+				out relativeSidePosition, out relativeEdgePosition);
+
+			RelativeSidePosition expectedSidePositon = Helper.GetRotationNeutralRelativeSidePosition(cube.Top, RelativeSidePosition.Bottom, Helper.ConvertRelativeSidePositionToRelativeEdgePosition(side));
+			RelativeEdgePosition expectedEdgePosition = RelativeEdgePosition.Left;
+
+			if (relativeSidePosition == expectedSidePositon && relativeEdgePosition == expectedEdgePosition)
+				return 1;
+			else
+				return 0;
+		}
+	}
+}
