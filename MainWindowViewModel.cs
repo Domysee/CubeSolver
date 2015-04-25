1a9292c482647870437e00f89b279cea1cde9fe6
domysee <me@dominik-weber.name>
2015-04-26 00:05:26 +0100

    added StartCrossMove10

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -397,7 +397,7 @@ namespace RubiksCubeSolver
 		{
 			test = new RelayCommand((param) =>
 			{
-				var move = new StartCrossMove9();
+				var move = new StartCrossMove10();
 				foreach (RelativeSidePosition relativeSidePosition in Enum.GetValues(typeof(RelativeSidePosition)))
 				{
 					if (relativeSidePosition != RelativeSidePosition.NotExisting && 
