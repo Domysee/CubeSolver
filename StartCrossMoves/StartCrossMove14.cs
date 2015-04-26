d62ca4ba0f2f948ca7eeeea105dba3833e6f0131
domysee <me@dominik-weber.name>
2015-04-26 15:44:09 +0100

    added StartCrossMove14

---
+++ StartCrossMoves/StartCrossMove14.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCubeSolver.StartCrossMoves
+{
+	/// <summary>
+	/// on the right side, edge top
+	/// </summary>
+	public class StartCrossMove14 : IStartCrossMove
+	{
+		public void Apply(Cube cube, RelativeSidePosition side)
+		{
+			cube.RotateSideCCW(Helper.GetRelativeSide(Helper.GetRelativeSide(Sides.Top, side), RelativeSidePosition.Right));
+			cube.RotateSideCCW(Helper.GetRelativeSide(Sides.Top, side));
+		}
+
+		public double Applicable(Cube cube, RelativeSidePosition side)
+		{
+			RelativeSidePosition relativeSidePosition;
+			RelativeEdgePosition relativeEdgePosition;
+			cube.GetRelativeEdgePosition(cube.Top.CubeSide, cube.Top.Color, cube.Top.GetRelativeSide(side).Color,
+				out relativeSidePosition, out relativeEdgePosition);
+
+			RelativeSidePosition expectedSidePositon = Helper.GetRotationNeutralRelativeSidePosition(cube.Top, RelativeSidePosition.Right, Helper.ConvertRelativeSidePositionToRelativeEdgePosition(side));
+			RelativeEdgePosition expectedEdgePosition = RelativeEdgePosition.Top;
+
+			if (relativeSidePosition == expectedSidePositon && relativeEdgePosition == expectedEdgePosition)
+				return 1;
+			else
+				return 0;
+		}
+	}
+}
