0f9cfcfe08a47082d73750f4200df3f486200add
domysee <me@dominik-weber.name>
2015-05-06 21:34:18 +0100

    GitFlow did some shit and removed all the code I added for rotating, this code is now added again

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,9 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -28,6 +30,11 @@ namespace RubiksCubeSolver
 
 			viewmodel = new MainWindowViewModel();
 			this.DataContext = viewmodel;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			(this.Resources["RotateBackCW"] as Storyboard).Begin();
 		}
 	}
 }
