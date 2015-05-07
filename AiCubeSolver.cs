0963d8c67fe8e6ca7cca16dee6e60c0ed805875e
domysee <me@dominik-weber.name>
2015-05-08 00:39:54 +0100

    added AiCubeSolver with applyAction and applyOppositeAction methods

---
+++ AiCubeSolver.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCubeSolver
+{
+	public class AiCubeSolver
+	{
+		public void SolveTopCross(Cube cube)
+		{
+			var actionStack = new Stack<int>();
+
+		}
+
+		private void applyAction(Cube cube, int turnId)
+		{
+			Debug.Assert(turnId < 12 && turnId >= 0);
+
+			switch (turnId)
+			{
+				case 0:
+					cube.RotateBackCCWAsync().Wait();
+					break;
+				case 1:
+					cube.RotateBackCWAsync().Wait();
+					break;
+				case 2:
+					cube.RotateBottomCCWAsync().Wait();
+					break;
+				case 3:
+					cube.RotateBottomCWAsync().Wait();
+					break;
+				case 4:
+					cube.RotateFrontCCWAsync().Wait();
+					break;
+				case 5:
+					cube.RotateFrontCWAsync().Wait();
+					break;
+				case 6:
+					cube.RotateLeftCCWAsync().Wait();
+					break;
+				case 7:
+					cube.RotateLeftCWAsync().Wait();
+					break;
+				case 8:
+					cube.RotateRightCCWAsync().Wait();
+					break;
+				case 9:
+					cube.RotateRightCWAsync().Wait();
+					break;
+				case 10:
+					cube.RotateTopCCWAsync().Wait();
+					break;
+				case 11:
+					cube.RotateTopCWAsync().Wait();
+					break;
+			}
+		}
+
+		private void applyOppositeAction(Cube cube, int turnId)
+		{
+			Debug.Assert(turnId < 12 && turnId >= 0);
+
+			switch (turnId)
+			{
+				case 0:
+					cube.RotateBackCWAsync().Wait();
+					break;
+				case 1:
+					cube.RotateBackCCWAsync().Wait();
+					break;
+				case 2:
+					cube.RotateBottomCWAsync().Wait();
+					break;
+				case 3:
+					cube.RotateBottomCCWAsync().Wait();
+					break;
+				case 4:
+					cube.RotateFrontCWAsync().Wait();
+					break;
+				case 5:
+					cube.RotateFrontCCWAsync().Wait();
+					break;
+				case 6:
+					cube.RotateLeftCWAsync().Wait();
+					break;
+				case 7:
+					cube.RotateLeftCCWAsync().Wait();
+					break;
+				case 8:
+					cube.RotateRightCWAsync().Wait();
+					break;
+				case 9:
+					cube.RotateRightCCWAsync().Wait();
+					break;
+				case 10:
+					cube.RotateTopCWAsync().Wait();
+					break;
+				case 11:
+					cube.RotateTopCCWAsync().Wait();
+					break;
+			}
+		}
+	}
+}
