105901fb641869dda260b554be8a9100fefcc63c
domysee <me@dominik-weber.name>
2015-05-13 09:47:21 +0100

    added StartEdgeMoves to Moves class

--- Moves.cs
+++ Moves.cs
@@ -1,4 +1,5 @@
 ﻿using RubiksCubeSolver.StartCrossMoves;
+using RubiksCubeSolver.StartEdgeMove;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,8 @@ namespace RubiksCubeSolver
 	public static class Moves
 	{
 		public static readonly IStartCrossMove[] StartCrossMoves = new IStartCrossMove[] { new StartCrossMove1(), new StartCrossMove2(), new StartCrossMove3(), new StartCrossMove4(), new StartCrossMove5(), new StartCrossMove6(), new StartCrossMove7(), new StartCrossMove8(), new StartCrossMove9(), new StartCrossMove10(), new StartCrossMove11(), new StartCrossMove12(), new StartCrossMove13(), new StartCrossMove14(), new StartCrossMove15(), new StartCrossMove16(), new StartCrossMove17(), new StartCrossMove18(), new StartCrossMove19(), new StartCrossMove20(), new StartCrossMove21(), new StartCrossMove22(), new StartCrossMove23(), };
+
+		public static readonly IStartEdgeMove[] StartEdgeMoves = new IStartEdgeMove[] { new StartEdgeMove1(), new StartEdgeMove2(), new StartEdgeMove3() };
+		public static readonly IStartEdgeMove StartEdgeMoveDownMove = new StartEdgeMove4();
 	}
 }
