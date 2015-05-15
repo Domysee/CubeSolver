1791a8c4c54fbf630d6d0b49be7f18be74db817c
domysee <me@dominik-weber.name>
2015-05-15 09:53:00 +0100

    added HumanCubeSolver3x3.SolveSecondLayer()

--- HumanCubeSolver3x3.cs
+++ HumanCubeSolver3x3.cs
@@ -27,7 +27,7 @@ namespace RubiksCubeSolver
 		{
 			while (!cube.IsTopEdgesSolved())
 			{
-
+				//3 is the limit because after 3 rotations of the bottom side it will be as before (4 rotations have no effect)
 				for (int i = 0; i < 3; i++)
 				{
 					//first, move any edges that are in the correct bottom position on the correct top position
@@ -62,6 +62,50 @@ namespace RubiksCubeSolver
 					if (Moves.StartEdgeMoveDownMove.Applicable(cube, corner) == 1)
 					{
 						Moves.StartEdgeMoveDownMove.Apply(cube, corner);
+					}
+				}
+			}
+		}
+
+		public void SolveSecondLayer(Cube cube)
+		{
+			while (!cube.IsSecondLayerSolved())
+			{
+				//3 is the limit because after 3 rotations of the bottom side it will be as before (4 rotations have no effect)
+				for (int i = 0; i < 3; i++)
+				{
+					//first, move any edges that are in the correct bottom position on the correct top position
+					//go through every edge
+					foreach (var side in new Sides[] { Sides.Front, Sides.Left, Sides.Back, Sides.Right })
+					{
+						//go through every possible move
+						foreach (var move in Moves.SecondLayerEdgeMoves)
+						{
+							if (move.Applicable(cube, side) == 1)
+							{
+								move.Apply(cube, side);
+							}
+						}
+					}
+
+					if (cube.IsSecondLayerSolved())
+					{
+						break;
+					}
+					else
+					{
+						//rotate the bottom side, maybe then there will be edge pieces in the correct position
+						cube.RotateBottomCW();
+					}
+				}
+
+				//move edge pieces that are in the correct position but wrongly rotated to the bottom
+				//if there are no such pieces then the moves are not applied, therefore no check for the solved state is needed
+				foreach (var side in new Sides[] { Sides.Front, Sides.Left, Sides.Back, Sides.Right })
+				{
+					if (Moves.SecondLayerOutMove.Applicable(cube, side) == 1)
+					{
+						Moves.SecondLayerOutMove.Apply(cube, side);
 					}
 				}
 			}
