9a089f38d2d0acf7af55d67ad952d7d1312ba41f
domysee <me@dominik-weber.name>
2015-05-17 12:18:02 +0100

    added ThirdLayerCrossMove2

---
+++ ThirdLayerCrossMoves/ThirdLayerCrossMove2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCubeSolver.ThirdLayerCrossMoves
+{
+	public class ThirdLayerCrossMove2 : IThirdLayerCrossMove
+	{
+		/// <summary>
+		/// represents the move to build the cross from a line
+		/// </summary>
+		/// <param name="cube"></param>
+		public void Apply(Cube cube)
+		{
+			cube.RotateFrontCW();
+			cube.RotateLeftCW();
+			cube.RotateBottomCW();
+			cube.RotateLeftCCW();
+			cube.RotateBottomCCW();
+			cube.RotateFrontCCW();
+		}
+
+		public double Applicable(Cube cube)
+		{
+			if (cube.Bottom.Fields[0, 1] != cube.Bottom.Color && cube.Bottom.Fields[1, 0] == cube.Bottom.Color &&
+				   cube.Bottom.Fields[1, 2] == cube.Bottom.Color && cube.Bottom.Fields[2, 1] != cube.Bottom.Color)
+			{
+				return 1;
+			}
+			else if (cube.Bottom.Fields[0, 1] != cube.Bottom.Color && cube.Bottom.Fields[1, 0] != cube.Bottom.Color &&
+				 cube.Bottom.Fields[1, 2] != cube.Bottom.Color && cube.Bottom.Fields[2, 1] != cube.Bottom.Color)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+}
