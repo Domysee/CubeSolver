156f540afd1b5d3b1ad9e2f66cd5e11fded79a65
domysee <me@dominik-weber.name>
2015-05-17 12:16:29 +0100

    added IThirdLayerCrossMove added ThirdLayerCrossMove1

---
+++ IThirdLayerCrossMove.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCubeSolver
+{
+	/// <summary>
+	/// represents a set of moves to solve the cross of the third layer
+	/// </summary>
+	public interface IThirdLayerCrossMove
+	{
+		/// <summary>
+		/// applies the move onto the given cube
+		/// </summary>
+		/// <param name="cube"></param>
+		/// <param name="side"></param>
+		void Apply(Cube cube);
+
+		/// <summary>
+		/// returns a applicability factor 
+		/// 1 = this move will do what it is supposed to do
+		/// 0 = this move has a negative effect
+		/// 0.5 = this move will do nothing				
+		/// </summary>
+		/// <param name="cube"></param>
+		/// <param name="side"></param>
+		/// <returns></returns>
+		double Applicable(Cube cube);
+	}
+}
--- ThirdLayerCrossMoves/ThirdLayerCrossMove1.cs
+++ ThirdLayerCrossMoves/ThirdLayerCrossMove1.cs
@@ -6,8 +6,38 @@ using System.Threading.Tasks;
 
 namespace RubiksCubeSolver.ThirdLayerCrossMoves
 {
-	class ThirdLayerCrossMove1
+	public class ThirdLayerCrossMove1 : IThirdLayerCrossMove
 	{
+		/// <summary>
+		/// represents the move to build the cross from an L-shape
+		/// </summary>
+		/// <param name="cube"></param>
+		public void Apply(Cube cube)
+		{
+			cube.RotateFrontCW();
+			cube.RotateBottomCW();
+			cube.RotateLeftCW();
+			cube.RotateBottomCCW();
+			cube.RotateLeftCCW();
+			cube.RotateFrontCCW();
+		}
 
+		public double Applicable(Cube cube)
+		{
+			if (cube.Bottom.Fields[0, 1] == cube.Bottom.Color && cube.Bottom.Fields[1, 0] == cube.Bottom.Color &&
+				   cube.Bottom.Fields[1, 2] != cube.Bottom.Color && cube.Bottom.Fields[2, 1] != cube.Bottom.Color)
+			{
+				return 1;
+			}
+			else if (cube.Bottom.Fields[0, 1] != cube.Bottom.Color && cube.Bottom.Fields[1, 0] != cube.Bottom.Color &&
+				 cube.Bottom.Fields[1, 2] != cube.Bottom.Color && cube.Bottom.Fields[2, 1] != cube.Bottom.Color)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
 	}
 }
