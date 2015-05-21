a7d7a6e33758475f4e7fdaf2c5b3bc06dab88867
domysee <me@dominik-weber.name>
2015-05-21 19:54:01 +0100

    added Cube.IsSolved()

--- Cube.cs
+++ Cube.cs
@@ -817,6 +817,21 @@ namespace RubiksCubeSolver
 
 			return true;
 		}
+
+		public bool IsSolved()
+		{
+			foreach (var side in CubeSides)
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					for (int j = 0; j < 3; j++)
+					{
+						if (side.Fields[i, j] != side.Color) return false;
+					}
+				}
+			}
+			return true;
+		}
 		#endregion
 	}
 }
